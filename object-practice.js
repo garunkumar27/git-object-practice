568033dc033fd0a8e8cee7cfb47e3a821323a767
arun kumar <garunk.27@gmail.com>
2022-08-11 17:24:04 +0530

    Objects & its Properties.

---
+++ object-practice.js
@@ -0,0 +1,99 @@
+// Printing is Achieved by two methods ,
+// 1.xyz.brand()
+// 2.xyz[]
+// (property:value)
+
+// 1.Watch
+let a={brand:"Fastrack",model:"Party look",type:"Analog",color:"Silver"};
+console.log("The Watch is",a.brand,a.model,a.type,a.color);//Method-1
+console.log(a);
+
+// 2.Bike
+let b={brand:"Yamaha",model:"RX-135",type:"Petrol & 2T-oil type",color:"Black"};
+console.log("The Bike is",b.brand,b.model,b.type,b.color);
+console.log(b);
+
+// 3.Pen
+let c={brand:"Luxar",model:"Fine Writer 05",color:"Blue ink"}
+console.log(c["brand"],c["model"],c["color"]);//Method 2
+
+// 4.Laptop
+let lap={brand:"Lenova",model:"E4325",color:"Black",price:"40000"}
+
+// 5.Television
+let tv={brand:"Mi",modelnumber:"L32M6-RA",type:"LED",price:"50000"}
+
+// 6.Shoes
+let shoe={brand:"Bata",type:"Cut-Shoe",color:"Brown",price:"1500"}
+
+// 7.Chappals
+let chappal={brand:"Woodland",type:"Leather",Price:"2500"}
+
+// 8.Mobiles
+let mobile={brand:"Mi",Model:"K20 pro",type:"Android",Price:"22000"}
+
+// 9.Tractors
+let tractor={brand:"John deere",Model:"5310",color:"Red",price:"1500000"}
+
+// 10.Headset
+let headset={brand:"Skull-Candy",type:"Bluetooth headset",price:"4500"}
+
+// 11.Shirt
+let shirt={brand:"Raymond",type:"Party wear",color:"Black",price:"4000"}
+
+// 12.Bag
+let bag={brand:"Skybags",type:"Water-proof with hifi look",color:"Black & violet",price:"3000"}
+
+// 13.computer
+let pc={brand:"Intel",displaysize:"22 inches display",Processor:"'core i3 11th generation'",price:"25000"}
+
+// 14.Bulb
+let bulb={brand:"philips",volt:"24v",price:"450",type:"plastic-material"}
+
+// 15.Notebook
+let note={brand:"Classmate",type:"Unruled longsize",price:"Rs.50"}
+
+// 16.Keyboard
+let keyboard={brand:"prolite",keycolor:"Black & White",price:"300",connection:"wired"}
+
+// 17.Chocolate
+let chocalate={brand:"Dairy Milk",Type:"Badam with chocolate",price:"125"}
+
+// 18.Ice-Cream
+let ice={brand:"Amul",Flavour:"Oreo with double Choco",Price:"60"}
+
+// 19.Ice-Cream
+let icecream={brand:"Arun",Flavour:"Raspberry",price:"60"}
+
+// 20.Tyre
+let tyre={brand:"MRF",Number:"All type",Typeofvehicle:"Two-wheelers &Four-wheelers"}
+
+// 21.Cricket-Ball
+let ball={brand:"stumper",color:"Red",price:"40",Material:"Rubber"}
+
+// 22.Cricket-Bat
+let bat={brand:"MRF",Model:"Leather type",Materail:"English villow",price:"3000"}
+
+// 23.Eye Glass
+let ={brand:"Fossil",model:"Rb 3025 004",Type:"Polaized",price:"2500"}
+
+// 24.Liquor
+let liquor={brand:"Jack Daniels",Type:"Brandy",Alcohol:"40%",price:"3500"}
+
+// 25.cigratte
+let cigratte={brand:"Gold-Flake",type:"Long-shot",price:"180 per box"}
+
+// 26.Water Bottle
+let Bottle={brand:"Tupperware",Type:"High Plastic",Resistant:"Hot & Cold",price:"450"}
+
+// 27.Fan
+let fan={brand:"Usha",Type:"Long leaf",color:"Golden",price:"4500"}
+
+// 28.Coffee-Cup
+let cup={brand:"Aldivo",Material:"Galzed ceramic",color:"BLack and White",price:"800"}
+
+// 29.Pvc-Pipe
+let pipe={brand:"Aqua",Size:"All size(mm)",type:"Plastic",Price:"Differ based on their size"}
+
+// 30.Cooldrinks
+let drinks={brand:"Apple Fizz",Quantity:"1000ml",Flavour:"Apple",Price:"40"}
